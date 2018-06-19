f2758b8f164a8c42a95e12ae96616f389ad92f30
rtx04 <krystian.gaczynski@gmail.com>
2018-06-19 08:37:44 +0200

    test

--- api/host.php
+++ api/host.php
@@ -1,8 +1,4 @@
 <?php
-return[
-    'db' => 'ika',
-    'host' => 'localhost',
-    'charset' => 'utf8',
-    'user' => 'root',
-    'password' => ''
-];
+$connect = parse_url(getenv('DATABASE_URL'));
+$dbname = ltrim($connect["path"],'/');
+    echo "pgsql:host=$connect['host'];port=$connect['port'];dbname=$dbname;user=$connect['user'];password=$connect['pass']";
