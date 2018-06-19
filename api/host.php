c2ace68b53890db8400ae067b37dc332dffd043b
rtx04 <krystian.gaczynski@gmail.com>
2018-06-19 08:42:39 +0200

    test2

--- api/host.php
+++ api/host.php
@@ -1,4 +1,4 @@
 <?php
 $connect = parse_url(getenv('DATABASE_URL'));
 $dbname = ltrim($connect["path"],'/');
-    echo "pgsql:host=$connect['host'];port=$connect['port'];dbname=$dbname;user=$connect['user'];password=$connect['pass']";
+    echo "pgsql:host=" . $connect['host'] . ";port=" . $connect['port'] . ";dbname=" . $dbname . ";user=" . $connect['user'] . ";password=" . $connect['pass'];
