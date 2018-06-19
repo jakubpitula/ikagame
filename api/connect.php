1fb39ef7e37598de544c2ef4d2f50d2dea9818ab
rtx04 <krystian.gaczynski@gmail.com>
2018-06-19 08:47:15 +0200

    fix

--- api/connect.php
+++ api/connect.php
@@ -4,8 +4,8 @@
 $connect = parse_url(getenv('DATABASE_URL'));
 $dbname = ltrim($connect["path"],'/');
 try{
-    echo "pgsql:host=$connect['host'];port=$connect['port'];dbname=$dbname;user=$connect['user'];password=$connect['pass']";
-    $db = new PDO("pgsql:host=$connect['host'];port=$connect['port'];dbname=$dbname;user=$connect['user'];password=$connect['pass']");
+    $pdstring = "pgsql:host=" . $connect['host'] . ";port=" . $connect['port'] . ";dbname=" . $dbname . ";user=" . $connect['user'] . ";password=" . $connect['pass'];
+    $db = new PDO($pdstring);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
--- api/host.php
+++ api/host.php
@@ -1,4 +1,8 @@
 <?php
-$connect = parse_url(getenv('DATABASE_URL'));
-$dbname = ltrim($connect["path"],'/');
-    echo "pgsql:host=" . $connect['host'] . ";port=" . $connect['port'] . ";dbname=" . $dbname . ";user=" . $connect['user'] . ";password=" . $connect['pass'];
+return[
+    'db' => 'ika',
+    'host' => 'localhost',
+    'charset' => 'utf8',
+    'user' => 'root',
+    'password' => ''
+];
