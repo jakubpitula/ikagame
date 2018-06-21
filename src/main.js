8a15263420a3bf906c3ecc1f653316bcfa6495f5
rtx04 <krystian.gaczynski@gmail.com>
2018-06-21 16:56:48 +0200

    added plugin

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueVisible from 'vue-visible';
+
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+Vue.use(VueVisible);
 new Vue({
   el: '#app',
   components: { App },
